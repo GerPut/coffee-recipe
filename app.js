6e6dbb7c3f46ea4bff9d66d853a36519f1725d99
Gerhard <gerhard.putter@yahoo.com>
2022-01-18 20:48:37 +0200

    Added functionality.

---
+++ app.js
@@ -0,0 +1,39 @@
+const recipeForm = document.querySelector('#recipe-form');
+const recipeContainer = document.querySelector('#recipe-container');
+let listItems = [];
+
+function handleFormSubmit(event) {
+    event.preventDefault()
+    const name = recipeForm.querySelector('#name').value;
+    const method = recipeForm.querySelector('#method').value;
+    const roast = recipeForm.querySelector('#roast').value;
+    const grind = recipeForm.querySelector('#grind').value;
+    const ratio = recipeForm.querySelector('#ratio').value;
+    const note = recipeForm.querySelector('#note').value;
+
+    const newRecipe = {
+        name,
+        method,
+        roast,
+        grind,
+        ratio,
+        note,
+        id: Date.now(),
+    }
+    listItems.push(newRecipe)
+    event.target.reset()
+    displayRecipes()
+}
+
+function displayRecipes() {
+    const tempString = listItems.map((item) => `
+    <div class="col>
+    <div class="card mb-4 rounded-3 shadow-sm border-primary"> 
+    <div class="card-header py-3 text-white bg-primary border-primary>
+    <h4 class="my-0">${item.name}</h4>
+    </div>
+    </div>  
+    </div>`)
+}
+
+recipeForm.addEventListener('submit', handleFormSubmit)
