f99cd7f25d89b4ea9663801b697281cf7410acec
Gerhard <gerhard.putter@yahoo.com>
2022-01-20 20:19:54 +0200

    Added html for recipe container.

--- app.js
+++ app.js
@@ -32,8 +32,19 @@ function displayRecipes() {
     <div class="card-header py-3 text-white bg-primary border-primary>
     <h4 class="my-0">${item.name}</h4>
     </div>
+    <div class="card-body">
+    <ul class="text-start>
+    <li><strong>Method: </strong>${item.method}</li>
+    <li><strong>Roast: </strong>${item.roast}</li>
+    <li><strong>Grind: </strong>${item.grind}</li>
+    <li><strong>Ratio: </strong>${item.ratio}</li>
+    ${!item.note.length ? "" : `<li><strong>Note: </strong>${item.note}</li>`}
+    </ul>
+    <button class="btn btn-lg btn-outline-danger" aria-label='Delete ${item.name} value="${item.id}"></button>
+    </div>
     </div>  
-    </div>`)
+    </div>`).join('');
+    recipeContainer.innerHTML = tempString
 }
 
 recipeForm.addEventListener('submit', handleFormSubmit)
